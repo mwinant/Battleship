f48759c604c17a4ec57b8a85c7788b0a4fd00779
Wade Aiello <waiello@Wades-MacBook-Pro.local>
2023-03-21 16:22:16 -0700

    variables

--- battleship.h
+++ battleship.h
@@ -8,4 +8,14 @@
 #include <cstdlib>
 using namespace std;
 
+// Global variables and arrays useful in the program
+const int NUM_ROWS = 10;  // number of rows of the battleship game board
+const int NUM_COLS = 10;  // number of columns of the battleship game board
+const int NUM_SHIPS = 5;  // number of ships in the battelship game
+
+// Each of the ships information in 3 arrays (parallel arrays)
+const string SHIP_NAMES[] = {"carrier", "battleship", "cruiser", "submarine", "destroyer"};
+const char SHIP_SYMBOLS[] = {'c', 'b', 'r', 's', 'd'};
+const int SHIP_SIZES[] = {5, 4, 3, 3, 2};
+
 #endif
